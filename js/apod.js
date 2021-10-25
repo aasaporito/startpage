4eaf390d228d462bbf3e72ca82650979d47e9038
sapblatt11 <sapblatt11@gmail.com>
2021-10-25 18:02:58 -0400

    Added live APOD

---
+++ js/apod.js
@@ -0,0 +1,24 @@
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = config.NASA_API_KEY
+
+const fetchData = async () => {
+	try {
+		console.log(`${url}${api_key}`)
+		const response = await fetch(`${url}${api_key}`)
+		const data = await response.json()
+		console.log('NASA Apod Data: ', data)
+		displayData(data)
+	}
+	catch(error) {
+		console.log(error)
+	}
+}
+
+const displayData = data => {
+	document.getElementById('picture').src = data.hdurl
+	document.getElementById('explanation').textContent = data.title.toLowerCase()
+}
+
+
+fetchData()
+
---
+++ js/config.js
@@ -0,0 +1,3 @@
+const config = {
+	NASA_API_KEY: 'eHugEpDc8655xFKOmtGbTFTUvwgezPXLcTg4eb3o',
+}
